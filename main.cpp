33e173913bd054266c7bb9c68039173b0ff6ce74
ola1inbox <ola1@inbox.ru>
2025-08-20 23:44:30 +0300

    *

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     // If you do not need a running Qt event loop, remove the call
     // to a.exec() or use the Non-Qt Plain C++ Application template.
 
-    qDebug() << "Hello (v1) !!";
+    qDebug() << "Hello (v2) !!";
     a.quit();
 
     return 0;
